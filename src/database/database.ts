cada6aee53c82487495a2917c20957b4a92c095c
Cleyton Ett <c80668378@gmail.com>
2026-07-04 11:10:39 +0200

    Corrige

--- src/database/database.ts
+++ src/database/database.ts
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
     username: "root",
     password: "NQAJUhVSXbGgGUrurnvlEesXQikhLafU",
     database: "railway",
-    synchronize: false,
+    synchronize: true,
     logging: true,
     entities: [__dirname + arte ],
     subscribers: [],
