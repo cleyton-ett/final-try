1f8d99612528754ae4e9d04f7c6c6d1769fc2f1e
Cleyton Ett <c80668378@gmail.com>
2026-07-08 23:36:47 +0200

    lITLE CHANGE

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,12 @@ const upload = multer({storage})
 
 
 app.use(cors({
-    origin: "*",
+    origin: [
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://localhost:5173",
+        "https://astrobot-svp-configuration.netlify.app"
+    ],
     methods:["GET","POST"],
     allowedHeaders: ['Content-Type']
 }))
