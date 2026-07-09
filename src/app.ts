b4568469e7734f9e49cd5ec2a113957880af92f8
Cleyton Ett <c80668378@gmail.com>
2026-07-09 07:25:37 +0200

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,18 @@ const upload = multer({storage})
 
 
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:5173",
+        "http://localhost:5500",
+        "http://localhost:5173",
+        "https://astrobot-svp-configuration.netlify.app"
+    ],
+    methods:["GET","POST",'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+    credentials: true
+}))
 
 app.use(express.json())
 
